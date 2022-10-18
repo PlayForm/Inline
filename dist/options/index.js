13b905528e5757681fda8ed40aa6d10febd76865
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 12:47:44 +0300

    Cleanup

--- dist/options/index.js
+++ dist/options/index.js
@@ -1,1 +1,2 @@
-var t=()=>({path:"./dist/",critters:{preload:"swap",inlineFonts:!0,compress:!0,pruneSource:!0},logger:2});export{t as default};
+var t=()=>({path:"./dist/",critters:{preload:"swap",inlineFonts:!0,compress:!0,pruneSource:!0},logger:2});export { t as default };
+
