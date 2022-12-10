db39c4313d9b1388b87b046c2fb62f511b789ff7
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-10 14:26:14 +0200

    squash!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-import i from"@nikolarhristov/pipeline";var r=(t={})=>({name:"astro-critters",hooks:{"astro:build:done":async()=>{await new i(t).critters()}}});export{r as default};
+import{pipeline as i}from"@nikolarhristov/pipeline";var r=(t={})=>({name:"astro-critters",hooks:{"astro:build:done":async()=>{await new i(t).critters()}}});export{r as default};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { AstroIntegration } from "astro";
 
-import pipeline from "@nikolarhristov/pipeline";
+import { pipeline } from "@nikolarhristov/pipeline";
 import type { Options as PipelineOptions } from "@nikolarhristov/pipeline/dist/options/index.js";
 import type { Options as CrittersOptions } from "@nikolarhristov/pipeline/dist/options/lib/critters/index.js";
 
