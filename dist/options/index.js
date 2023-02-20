d8706738a544df250c3a20b8f58c76894718a056
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-20 02:32:42 +0200

    Remove Astro critters package

--- dist/options/index.js
+++
@@ -1,1 +0,0 @@
-import i from"files-pipeline/dist/lib/deepmerge.js";import s from"files-pipeline/dist/options/index.js";import t from"./critters.js";var n=i(s,{critters:t,pipeline:{failed:async e=>`Error: Cannot inline file ${e.inputPath}!`,fulfilled:async e=>e.files>0?`Successfully inlined a total of ${e.files} HTML ${e.files===1?"file":"files"}.`:!1,accomplished:!1}});export{n as default};
