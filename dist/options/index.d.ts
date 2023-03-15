a9084cd89cb262cfe009760f7b2ebbb830812a0b
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-15 08:08:51 +0200

    squash!

--- dist/options/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CRITTERS } from "./critters.js";
-import type { Options as OptionsBase } from "files-pipe/dist/options/index.js";
-export interface Options extends OptionsBase {
-    [key: string]: unknown;
-    critters?: boolean | CRITTERS;
-}
-declare const _default: Options;
-export default _default;
