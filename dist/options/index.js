c20254428e4adaed1e9ab89826f43229b30086c1
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 20:38:19 +0300

    squash!

---
+++ dist/options/index.js
@@ -0,0 +1,1 @@
+import i from"files-pipe/dist/lib/deepmerge.js";import s from"files-pipe/dist/options/index.js";import t from"./critters.js";var l=i(s,{critters:t,pipe:{failed:async e=>`Error: Cannot inline file ${e.inputPath}!`,fulfilled:async e=>e.files>0?`Successfully inlined a total of ${e.files} HTML ${e.files===1?"file":"files"}.`:!1,accomplished:!1}});export{l as default};
