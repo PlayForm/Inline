ff72e0c7eac9a9c0c059a5b7521423c67dac2409
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 19:03:59 +0300

    squash!

--- Target/Option/Index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Option as _Option } from "files-pipe";
-import type { Critters } from "./Critters.js";
-export interface Option extends _Option {
-    [key: string]: any;
-    Critters?: boolean | Critters;
-}
-declare const _default: Option;
-export default _default;
