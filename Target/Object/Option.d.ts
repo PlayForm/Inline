2b867c09c080867c9b8aa44ac3c104d79747b78f
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-28 16:32:12 +0300

    squash!

--- Target/Object/Option.d.ts
+++ Target/Object/Option.d.ts
@@ -10,13 +10,15 @@ declare const _default: Omit<{} & {
         Folder: string;
     };
     Path: string;
+    Exclude: false;
+    Files: string;
     Action: Omit<{} & {
         Accomplished: false;
+        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
         Read: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Wrote: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
         Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
-        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
         Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
     }, "__proto__">;
     Logger: 2;
