a83d60629db906019558b14224555a8059421f12
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-05 02:03:28 +0300

    squash!

--- Target/Index.d.ts
+++ Target/Index.d.ts
@@ -1,6 +1,30 @@
 import type Option from "./Interface/Option.js";
 import type { AstroIntegration } from "astro";
-export declare const Default: any;
-export declare const Merge: any;
+export declare const Default: Omit<{} & {
+    Critters: {
+        preload: string;
+        inlineFonts: boolean;
+        compress: boolean;
+        pruneSource: boolean;
+    };
+    Cache: {
+        Search: string;
+        Folder: string;
+    };
+    Path: string;
+    Exclude: false;
+    Files: string;
+    Action: Omit<{} & {
+        Accomplished: false;
+        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Read: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Wrote: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
+        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
+        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
+    }, "__proto__">;
+    Logger: 2;
+}, "__proto__">;
+export declare const Merge: import("files-pipe/Target/Interface/Merge.js").default<import("files-pipe/Target/Function/Merge.js").PMF>;
 declare const _default: (_Option?: Option) => AstroIntegration;
 export default _default;
