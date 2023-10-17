2188bd14de68e6fb1f6fbb71d9032cf937dc12ee
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-17 13:36:19 +0300

    squash!

--- Target/Index.d.ts
+++ Target/Index.d.ts
@@ -18,9 +18,9 @@ export declare const Default: Omit<{} & {
         Accomplished: false;
         Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Read: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
-        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
+        Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
+        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
         Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
     }, "__proto__">;
     Logger: 2;
