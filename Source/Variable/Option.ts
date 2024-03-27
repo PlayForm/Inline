b4cca7136aafd3b5c02b35317aec8657d31fc192
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-27 02:57:46 +0200

    squash!

--- Source/Variable/Option.ts
+++ Source/Variable/Option.ts
@@ -2,20 +2,21 @@
  * @module Option
  *
  */
-export default (
-	await import("../Function/Merge.js")
-).default((await import("files-pipe/Target/Variable/Option.js")).default, {
-	Critters: (await import("../Variable/Critters.js")).default,
-	Action: {
-		Failed: async (On) => `Error: Cannot inline file ${On.Input}!`,
-		Fulfilled: async ({ File }) =>
-			File > 0
-				? `Successfully inlined a total of ${File} HTML ${
-						File === 1 ? "file" : "files"
-				  }.`
-				: false,
-		Accomplished: false,
-	},
-} satisfies Type);
+export default (await import("../Function/Merge.js")).default(
+	(await import("files-pipe/Target/Variable/Option.js")).default,
+	{
+		Critters: (await import("../Variable/Critters.js")).default,
+		Action: {
+			Failed: async (On) => `Error: Cannot inline file ${On.Input}!`,
+			Fulfilled: async ({ File }) =>
+				File > 0
+					? `Successfully inlined a total of ${File} HTML ${
+							File === 1 ? "file" : "files"
+					  }.`
+					: false,
+			Accomplished: false,
+		},
+	} satisfies Type,
+);
 
 import type Type from "../Interface/Option.js";
