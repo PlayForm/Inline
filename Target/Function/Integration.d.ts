623dd4dd8d39c9f1d7e0716f63686c26e71375bc
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-01 14:07:43 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -21,12 +21,12 @@ export declare const Default: Omit<{} & {
     Logger: 2;
     Action: Omit<{} & {
         Accomplished: false;
-        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Fulfilled: ({ File }: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Type/Buffer.js").Type>;
-        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
-        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
+        Failed: (On: import("@playform/file-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/file-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Read: ({ Input }: any) => Promise<string>;
+        Wrote: ({ Buffer }: any) => Promise<any>;
+        Passed: (On: any) => Promise<true>;
+        Changed: (Plan: any) => Promise<any>;
     }, "__proto__">;
     Exclude: false;
 }, "__proto__">;
--- Target/Interface/Option.d.ts
+++ Target/Interface/Option.d.ts
@@ -6,4 +6,4 @@ export default interface Type extends Option {
     Critters?: boolean | Critters;
 }
 import type Critters from "../Interface/Critters.js";
-import type Option from "files-pipe/Target/Interface/Option.js";
+import type Option from "@playform/file-pipe/Target/Interface/Option.js";
