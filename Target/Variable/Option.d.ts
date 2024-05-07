94146815be87092d00ee71436f951b2bc9448818
Nikola Hristov <nikola@playform.cloud>
2024-05-07 16:37:56 +0300

    squash!

--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -3,28 +3,34 @@
  *
  */
 declare const _default: Omit<{} & {
+    File: string;
+    Cache: {
+        Search: string;
+        Folder: string;
+    };
+    Logger: number;
+    Action: Omit<{} & {
+        Accomplished: boolean;
+        Changed: (e: any) => Promise<any>;
+        Failed: (On: any) => Promise<string>;
+        Fulfilled: ({ File }: {
+            File: any;
+        }) => Promise<string | false>;
+        Passed: (e: any) => Promise<any>;
+        Read: ({ Input: e }: {
+            Input: any;
+        }) => Promise<string>;
+        Wrote: ({ Buffer: e }: {
+            Buffer: any;
+        }) => Promise<any>;
+    }, "__proto__">;
     Critters: {
         preload: "media";
         inlineFonts: true;
         compress: true;
         pruneSource: true;
     };
-    File: string;
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
     Path: string;
-    Logger: 2;
-    Action: Omit<{} & {
-        Accomplished: false;
-        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
-        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Read: ({ Input }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<import("@playform/pipe/Target/Type/Buffer.js").Type>;
-        Passed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<true>;
-        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<import("@playform/pipe/Target/Interface/Plan.js").default>;
-    }, "__proto__">;
-    Exclude: false;
+    Exclude: boolean;
 }, "__proto__">;
 export default _default;
