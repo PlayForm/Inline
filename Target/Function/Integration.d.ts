7d3e2015e71f42ef930c6dc9b015c2604c1069a2
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-08 03:39:48 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -6,35 +6,29 @@ declare const _default: Interface;
 export default _default;
 import type Interface from "../Interface/Integration.js";
 export declare const Default: Omit<{} & {
-    File: string;
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
-    Logger: number;
-    Action: Omit<{} & {
-        Accomplished: boolean;
-        Changed: (e: any) => Promise<any>;
-        Failed: (On: any) => Promise<string>;
-        Fulfilled: ({ File }: {
-            File: any;
-        }) => Promise<string | false>;
-        Passed: (e: any) => Promise<any>;
-        Read: ({ Input: e }: {
-            Input: any;
-        }) => Promise<string>;
-        Wrote: ({ Buffer: e }: {
-            Buffer: any;
-        }) => Promise<any>;
-    }, "__proto__">;
     Critters: {
         preload: "media";
         inlineFonts: true;
         compress: true;
         pruneSource: true;
     };
+    File: string;
+    Cache: {
+        Search: string;
+        Folder: string;
+    };
     Path: string;
-    Exclude: boolean;
+    Logger: 2;
+    Action: Omit<{} & {
+        Accomplished: false;
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Read: ({ Input }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<import("@playform/pipe/Target/Type/Buffer.js").Type>;
+        Passed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<true>;
+        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<import("@playform/pipe/Target/Interface/Plan.js").default>;
+    }, "__proto__">;
+    Exclude: false;
 }, "__proto__">;
 export declare const Search: string;
 export declare const Merge: import("../Interface/Merge.js").default<import("../Interface/Merge.js").Generic>;
