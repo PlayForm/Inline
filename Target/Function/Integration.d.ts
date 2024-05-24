ec9cb11064e31172446c05f736d68eb9bd0e6a1d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-25 01:18:13 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -5,6 +5,23 @@
 declare const _default: Interface;
 export default _default;
 import type Interface from "../Interface/Integration.js";
-export declare const Default: unknown;
+export declare const Default: {
+    Critters: import("../Interface/Critters.js").default;
+    Action: {
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Accomplished: false;
+    };
+};
 export declare const Search: any;
-export declare const Merge: import("../Interface/Merge.js").default<import("../Interface/Merge.js").Generic>;
+export declare const Merge: <Ts extends readonly unknown[]>(...objects: Ts) => import("deepmerge-ts").DeepMergeHKT<Ts, Readonly<{
+    DeepMergeRecordsURI: "DeepMergeRecordsDefaultURI";
+    DeepMergeArraysURI: "DeepMergeArraysDefaultURI";
+    DeepMergeSetsURI: "DeepMergeSetsDefaultURI";
+    DeepMergeMapsURI: "DeepMergeMapsDefaultURI";
+    DeepMergeOthersURI: "DeepMergeLeafURI";
+    DeepMergeFilterValuesURI: "DeepMergeFilterValuesDefaultURI";
+}>, Readonly<{
+    key: PropertyKey;
+    parents: readonly Readonly<Record<PropertyKey, unknown>>[];
+}>>;
--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -2,5 +2,12 @@
  * @module Option
  *
  */
-declare const _default: unknown;
+declare const _default: {
+    Critters: import("../Interface/Critters.js").default;
+    Action: {
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Accomplished: false;
+    };
+};
 export default _default;
