5125c78144ac4379da9272eb44a5c8665f6930d1
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-09 07:26:05 +0300

    AstroCritters/v2.1.10

--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -3,13 +3,23 @@
  *
  */
 declare const _default: {
-    Critters: import("../Interface/Critters.js").default;
-    Action: {
-        Failed: (On: any) => Promise<string>;
-        Fulfilled: ({ File }: {
-            File: any;
-        }) => Promise<string | false>;
-        Accomplished: boolean;
+    File: string;
+    Cache: {
+        Search: string;
+        Folder: string;
     };
+    Path: string;
+    Logger: 2;
+    Action: {
+        Accomplished: false;
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+        Read: ({ Input }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<import("@playform/pipe/Target/Type/Buffer.js").Type>;
+        Passed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<true>;
+        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<import("@playform/pipe/Target/Interface/Plan.js").default>;
+    };
+    Exclude: false;
+    Critters: import("../Interface/Critters.js").default;
 };
 export default _default;
